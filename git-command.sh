fe1113ff71d4737162a45cd14a319823a60bd2e3
yourUsername <yourEmail@example.com>
2026-08-30 02:49:27 -0500

    updated the repo with ECR terraform scripts and dummy app codes to test CI

--- git-command.sh
+++ git-command.sh
@@ -5,7 +5,7 @@ echo "Add files and do local commit"
 git add .
 echo "checking status"
 git status
-git commit -am "updated the repo with ECR terraform scripts"
+git commit -am "updated the repo with ECR terraform scripts and dummy app codes to test CI"
 
 echo "Pushing to Github Repository"
 git push -u origin master
