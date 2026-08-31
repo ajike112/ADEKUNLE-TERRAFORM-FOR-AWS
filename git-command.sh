f4dde86b4fb842c205e886764ea0fe65c43db7db
yourUsername <yourEmail@example.com>
2026-08-31 04:17:46 -0500

    updated Jenkinsfile script with Docker agent scripts

--- git-command.sh
+++ git-command.sh
@@ -5,7 +5,7 @@ echo "Add files and do local commit"
 git add .
 echo "checking status"
 git status
-git commit -am "updated the repo with SonarQube terraform scripts and Jenkinsfile"
+git commit -am "updated Jenkinsfile script with Docker agent scripts"
 
 echo "Pushing to Github Repository"
 git push -u origin master
